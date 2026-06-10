a3f89a6ec66ce501c5f4270b0dcce9e1a6ff62ae
NazarBondaruk <n07.bondaruk@gmail.com>
2026-06-10 11:48:51 +0300

    Fix: limit user input in DTOs

--- WatApi/WatApi/DTO/ImportantInfo/ImportantInfoCreateDto.cs
+++ WatApi/WatApi/DTO/ImportantInfo/ImportantInfoCreateDto.cs
@@ -1,11 +1,17 @@
-﻿namespace WatApi.DTO.ImportantInfo
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WatApi.DTO.ImportantInfo
 {
     public class ImportantInfoCreateDto
     {
+        [MaxLength(30, ErrorMessage = "Sevis ID can't exceed 30 characters!")]
         public string? SevisId { get; set; }
         public DateTime? VisaAppointment { get; set; }
         public DateTime? FlightDate { get; set; }
+        [MaxLength(30, ErrorMessage = "DS160 number can't exceed 30 characters!")]
         public string? Ds160 { get; set; }
+        [MaxLength(30, ErrorMessage = "DS2019 number can't exceed 30 characters!")]
         public string? Ds2019 { get; set; }
     }
 }
--- WatApi/WatApi/DTO/ImportantInfo/ImportantInfoUpdateDto.cs
+++ WatApi/WatApi/DTO/ImportantInfo/ImportantInfoUpdateDto.cs
@@ -1,11 +1,16 @@
-﻿namespace WatApi.DTO.ImportantInfo
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WatApi.DTO.ImportantInfo
 {
     public class ImportantInfoUpdateDto
     {
+        [MaxLength(30, ErrorMessage = "Sevis ID can't exceed 30 characters!")]
         public string? SevisId { get; set; }
         public DateTime? VisaAppointment { get; set; }
         public DateTime? FlightDate { get; set; }
+        [MaxLength(30, ErrorMessage = "DS160 number can't exceed 30 characters!")]
         public string? Ds160 { get; set; }
+        [MaxLength(30, ErrorMessage = "DS2019 number can't exceed 30 characters!")]
         public string? Ds2019 { get; set; }
     }
 }
