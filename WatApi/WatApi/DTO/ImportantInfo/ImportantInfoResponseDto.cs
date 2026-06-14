b61596e84b17636be5c66300432f1cfc981e2b7e
NazarBondaruk <n07.bondaruk@gmail.com>
2026-06-14 19:32:03 +0300

    Fix: change date types for resolving date conflict

--- WatApi/WatApi/DTO/ImportantInfo/ImportantInfoResponseDto.cs
+++ WatApi/WatApi/DTO/ImportantInfo/ImportantInfoResponseDto.cs
@@ -3,10 +3,10 @@
     public class ImportantInfoResponseDto
     {
         public string? SevisId { get; set; }
-        public DateTime? VisaAppointment { get; set; }
-        public DateTime? FlightDate { get; set; }
+        public DateOnly? VisaAppointment { get; set; }
+        public DateOnly? FlightDate { get; set; }
         public string? Ds160 { get; set; }
-        public DateTime? StartOfWork { get; set; }
-        public DateTime? EndOfWork { get; set; }
+        public DateOnly? StartOfWork { get; set; }
+        public DateOnly? EndOfWork { get; set; }
     }
 }
---
+++ WatApi/WatApi/Migrations/20260614163009_ChangeDateType.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WatApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDateType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "VisaAppointment",
+                table: "ImportantInfos",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "StartOfWork",
+                table: "ImportantInfos",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "Flight",
+                table: "ImportantInfos",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "EndOfWork",
+                table: "ImportantInfos",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "VisaAppointment",
+                table: "ImportantInfos",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartOfWork",
+                table: "ImportantInfos",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Flight",
+                table: "ImportantInfos",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndOfWork",
+                table: "ImportantInfos",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+        }
+    }
+}
