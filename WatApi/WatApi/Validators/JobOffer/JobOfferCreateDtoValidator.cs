1be59b3e52c8c3ac7581b7a32dbb0ec76fb96278
NazarBondaruk <n07.bondaruk@gmail.com>
2026-06-16 17:00:29 +0300

    Fix: validation for year field

--- WatApi/WatApi/Validators/JobOffer/JobOfferCreateDtoValidator.cs
+++ WatApi/WatApi/Validators/JobOffer/JobOfferCreateDtoValidator.cs
@@ -5,16 +5,19 @@ namespace WatApi.Validators.JobOffer
 {
     public class JobOfferCreateDtoValidator: AbstractValidator<JobOfferCreateDto>
     {
+        private readonly int _currentYear = new DateOnly().Year;
         public JobOfferCreateDtoValidator()
         {
             RuleFor(x => x.Position).NotEmpty().WithMessage("Position field can't be empty!");
             RuleFor(x => x.Employer).NotEmpty().WithMessage("Employer field can't be empty!");
             RuleFor(x => x.PlaceOfWork).NotEmpty().WithMessage("Location field can't be empty!");
             RuleFor(x => x.PayPerHour).GreaterThan(0)
-                .WithMessage("Hourly pay should be greater than 0");
+                .WithMessage("Hourly pay should be greater than zero");
+            RuleFor(x => x.Year).InclusiveBetween(_currentYear - 6, _currentYear + 1)
+                .WithMessage($"Year should be within {_currentYear - 6} and {_currentYear + 1}");
 
             RuleFor(x => x.HousingCostPerWeek).GreaterThanOrEqualTo(0)
-                .WithMessage("Housing price should be more than 0 or equal 0");
+                .WithMessage("Housing price should be more than or equal to zero");
 
             RuleFor(x => x.HousingCostPerWeek)
                 .NotNull()
--- WatApi/WatApi/Validators/JobOffer/JobOfferUpdateDtoValidator.cs
+++ WatApi/WatApi/Validators/JobOffer/JobOfferUpdateDtoValidator.cs
@@ -1,19 +1,23 @@
 ﻿using FluentValidation;
+using Microsoft.IdentityModel.Tokens;
 using WatApi.DTO.JobOffer;
 
 namespace WatApi.Validators.JobOffer
 {
     public class JobOfferUpdateDtoValidator: AbstractValidator<JobOfferUpdateDto>
     {
+        private readonly int _currentYear = new DateOnly().Year;
         public JobOfferUpdateDtoValidator() {
             RuleFor(x => x.Position).NotEmpty().WithMessage("Position field can't be empty!");
             RuleFor(x => x.Employer).NotEmpty().WithMessage("Employer field can't be empty!");
             RuleFor(x => x.PlaceOfWork).NotEmpty().WithMessage("Location field can't be empty!");
             RuleFor(x => x.PayPerHour).GreaterThan(0)
-                .WithMessage("Hourly pay should be greater than 0");
+                .WithMessage("Hourly pay should be greater than zero");
+            RuleFor(x => x.Year).InclusiveBetween(_currentYear - 6, _currentYear + 1)
+                .WithMessage($"Year should be within {_currentYear - 6} and {_currentYear + 1}");
 
             RuleFor(x => x.HousingCostPerWeek).GreaterThanOrEqualTo(0)
-                .WithMessage("Housing price should be more than 0 or equal 0");
+                .WithMessage("Housing price should be more than or equal to zero");
 
             RuleFor(x => x.HousingCostPerWeek)
                 .NotNull()
